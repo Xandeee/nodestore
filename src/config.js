42b860992165059d3370b94f3eb3558d386604c0
xandeee <ufscar.ale@gmail.com>
2018-12-03 00:33:03 -0200

    First Commit

---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = '893fc360-36d5-477a-a15b-0aa3a0208802';
+global.EMAIL_TMPL = 'Olá <strong>{0}</strong>, seja bem-vindo a Node Store!';
+
+module.exports = {
+    connectionString: 'mongodb://xandeee:xandeee1@ds044577.mlab.com:44577/ndstr',
+    sendGridKey: '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',
+    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=nodexandestore;AccountKey=ssMteVTKANCU7nQiN5HfS4iEjtDvm/X0Iasb8Ro/hVzpGlyoirwFKOCDu3x4VnqxKG5W7EFanjgYgOXAhx62SQ==;EndpointSuffix=core.windows.net'
+}
---
+++ src/repositories/customer-repository.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Customer = mongoose.model('Customer');
+
+exports.create = async (data) => {
+    const customer = new Customer(data);
+    await customer.save();
+}
+
+exports.authenticate = async (data) => {
+    const res = await Customer.findOne({
+        email: data.email,
+        password: data.password,
+        roles: data.roles
+    });
+    return res;
+}
+
+exports.getById = async (id) => {
+    const res = await Customer.findById(id);
+    return res;
+}
